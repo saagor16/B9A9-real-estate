571750414b959671d14b86c6374a46598b2f3a85
saagor16 <saagor.16@gmail.com>
2024-04-15 19:56:32 +0600

    SIngle Card DOne

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 import AOS from "aos";
@@ -10,9 +10,14 @@ const Navbar = () => {
   }, []);
 
   const { user, logOut } = useContext(AuthContext);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleLogOut = () => {
     logOut();
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
   const navLinks = (
@@ -39,107 +44,109 @@ const Navbar = () => {
           About Us
         </NavLink>
       </li>
-      {user ? (
-        <li>
-          <NavLink
-            to="/profile"
-            style={({ isActive }) => ({
-              color: isActive ? "#fff" : "",
-              background: isActive ? "#7600dc" : "",
-            })}
-          >
-            Profile
-          </NavLink>
-        </li>
-      ) : (
-        ""
-      )}
-      {user ? (
-        <li>
-          <NavLink
-            to="/updateProfile"
-            style={({ isActive }) => ({
-              color: isActive ? "#fff" : "",
-              background: isActive ? "#7600dc" : "",
-            })}
-          >
-            UpdateProfile
-          </NavLink>
-        </li>
-      ) : (
-        ""
+      {user && (
+        <>
+          <li>
+            <NavLink
+              to="/profile"
+              style={({ isActive }) => ({
+                color: isActive ? "#fff" : "",
+                background: isActive ? "#7600dc" : "",
+              })}
+            >
+              Profile
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/updateProfile"
+              style={({ isActive }) => ({
+                color: isActive ? "#fff" : "",
+                background: isActive ? "#7600dc" : "",
+              })}
+            >
+              Update Profile
+            </NavLink>
+          </li>
+        </>
       )}
     </>
   );
+
   return (
-    <div
-      data-aos="zoom-out-down"
-      data-aos-duration="2000"
-      className="navbar bg-base-100  container mx-auto"
-    >
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            {navLinks}
-          </ul>
-        </div>
-        <a>
-          {" "}
-          <span className="text-lg md:text-2xl lg:text-3xl font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient bg-500%">
-            realProperty
-          </span>
-        </a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{navLinks}</ul>
-      </div>
-      <div className="navbar-end">
-        {user ? (
-          <div className="flex items-center">
+    <div className="relative">
+      <div
+        data-aos="zoom-out-down"
+        data-aos-duration="2000"
+        className="navbar bg-base-100 container mx-auto"
+      >
+        <div className="navbar-start">
+          <div className="dropdown lg:hidden">
             <div
               tabIndex={0}
               role="button"
-              className="btn btn-ghost btn-circle avatar mr-4 tooltip tooltip-bottom"
-              data-tip={user.displayName}
+              className="btn btn-ghost"
+              onClick={toggleMenu}
             >
-              <div className="rounded-full">
-                <img alt={user.displayName} src={user.photoURL} />
-              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </div>
-            <button onClick={handleLogOut} className="btn btn-primary">
-              Logout
-            </button>
+            {menuOpen && (
+              <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                {navLinks}
+              </ul>
+            )}
           </div>
-        ) : (
-          <div className="flex items-center gap-2">
-            <Link to="/register">
-              <p className="btn btn-secondary">Register</p>
-            </Link>
-
-            <Link to="/login">
-              <p className="btn  btn-accent">Login</p>
-            </Link>
-          </div>
-        )}
+          <a href="/">
+            {" "}
+            <span className="text-lg md:text-2xl lg:text-3xl font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient bg-500%">
+              realProperty
+            </span>
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+        </div>
+        <div className="navbar-end">
+          {user ? (
+            <div className="flex items-center">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar mr-4 tooltip tooltip-bottom"
+                data-tip={user.displayName}
+              >
+                <div className="rounded-full">
+                  <img alt={user.displayName} src={user.photoURL} />
+                </div>
+              </div>
+              <button onClick={handleLogOut} className="btn btn-primary">
+                Logout
+              </button>
+            </div>
+          ) : (
+            <div className="flex items-center gap-2">
+              <Link to="/register">
+                <p className="btn btn-secondary">Register</p>
+              </Link>
+              <Link to="/login">
+                <p className="btn btn-accent">Login</p>
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/Single/Single.jsx
+++ src/Single/Single.jsx
@@ -1,16 +1,54 @@
 import { useLoaderData, useParams } from "react-router-dom";
 
 const Single = () => {
-
   const single = useLoaderData();
   const { id } = useParams();
   const idInt = parseInt(id);
   const card = single.find((card) => card.id == idInt);
- 
+
   return (
     <div className=" container mx-auto mt-10">
-      <h2>{card.id}</h2>
-     
+      <div className="text-center">
+        <h2>View Details:{card.estate_title}</h2>
+      </div>
+      <div className="border rounded-xl">
+        <section className="text-gray-700 body-font overflow-hidden bg-white">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="lg:w-4/5 mx-auto flex flex-wrap">
+              <figure className="px-10 pt-10">
+                <img src={card.image} alt="Image" className="rounded-xl" />
+              </figure>
+              <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 space-y-4">
+                <h1 className="text-gray-900 text-4xl title-font font-bold mb-1">
+                  {card.estate_title}
+                </h1>
+                <div className=" flex gap-8">
+                  <h4>Segment :{card.segment_name}</h4>
+                  <h4>Status:{card.status}</h4>
+                </div>
+                <div className=" flex gap-8">
+                  <h4>Price :{card.price}</h4>
+                  <h4>Area:{card.area}</h4>
+                </div>
+                <div className=" ">
+                  <h4>Location:{card.location}</h4>
+                </div>
+                <div className="gap-5">
+                  <p>facilities:{card.facilities[0]}</p>
+                  <p className="pl-14">{card.facilities[1]}</p>
+                  <p className="pl-14">{card.facilities[2]}</p>
+                </div>
+
+                <p className="leading-relaxed">Details : {card.description}</p>
+              </div>
+              <div className="flex gap-10 pt-5">
+                <p>Open:{card.opening_hours}</p>
+                <p>Rating:{card.rating}</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
